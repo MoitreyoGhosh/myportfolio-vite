ca49d7c35ff6e489d0dc8e0469481be5cf36dd4c
MoitreyoGhosh <moitreyoghosh@gmail.com>
2024-03-06 22:49:21 +0530

    Changed Style of Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,15 +3,15 @@ const Header = () => {
     <header className="sticky top-0 left-0 w-full flex justify-between items-center bg-[#191f36] py-5 px-[9%]">
         <a href="#" className=" text-[25px] font-bold text-white cursor-default">Portfolio</a>
 
-        <i className="fa-sharp fa-solid fa-bars" id="menu-icon"></i>
+        <i className=" fa-sharp fa-solid fa-bars  text-[3.6rem] " id="menu-icon"></i>
 
         <nav className="flex items-center justify-center gap-4 ">
-            <a href="#home" className='text-white hover:text-[#5982f4] hover:shadow-sm font-bold text-[15px]'>Home</a>
-            <a href="#about" className='text-white hover:text-[#5982f4] hover:shadow-sm font-bold text-[15px]'>About</a>
-            <a href="#skills" className='text-white hover:text-[#5982f4] hover:shadow-sm font-bold text-[15px]'>Skills</a>
-            <a href="#services" className='text-white hover:text-[#5982f4] hover:shadow-sm font-bold text-[15px]'>Services</a>
-            <a href="#portfolio" className='text-white hover:text-[#5982f4] shadow-sm font-bold text-[15px]'>Portfolio</a>
-            <a href="#contact" className='text-white hover:text-[#5982f4] shadow-sm font-bold text-[15px]'>Contact</a>
+            <a href="#home" className="text-white ml-16 hover:text-[#5982f4] hover:shadow-sm font-bold text-[15px]">Home</a>
+            <a href="#about" className="text-white ml-16 hover:text-[#5982f4] hover:shadow-sm font-bold text-[15px]">About</a>
+            <a href="#skills" className="text-white ml-16 hover:text-[#5982f4] hover:shadow-sm font-bold text-[15px]">Skills</a>
+            <a href="#services" className="text-white ml-16 hover:text-[#5982f4] hover:shadow-sm font-bold text-[15px]">Services</a>
+            <a href="#portfolio" className="text-white ml-16 hover:text-[#5982f4] shadow-sm font-bold text-[15px]">Portfolio</a>
+            <a href="#contact" className="text-white ml-16 hover:text-[#5982f4] shadow-sm font-bold text-[15px]">Contact</a>
         </nav>
     </header>
   )
